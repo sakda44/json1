916ad32b1a9b2ea03bad918eea9d325717e2b25c
sakda44 <86420617+sakda44@users.noreply.github.com>
2021-12-07 14:05:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,36 +10,62 @@
 
 </head>
 <body>
+<button id="btnBack"> back </button>
+<div id="main">
     <table>
         <thead>
             <tr>
-                <th>ID</th><th>Title</th><th>Author</th>
+                <th>ID</th> <th>Title</th><th> Details </th>
             </tr>
         </thead>
         <tbody id="tblPosts">
-
         </tbody>
     </table>
+</div>
+<div id="detail">
+    sssssss
+</div>
     
 </body>
 <script>
-    function lodePosts(){
+    function showDetails(id){
+        $("#main").hide();
+        $("#detail").show();
+        var url = "https://jsonplaceholder.typicode.com/posts/"+id;
+        $.getJSON(url)
+            .done((data)=>{
+                console.log(data);
+            })
+            .fail((xhr, status, error)=>{
+            })
+    }
+    function loadPosts(){
+        $("#main").show();
+        $("#details").hide();
+        
         var url = "https://jsonplaceholder.typicode.com/posts";
         $.getJSON(url)
-                .done((data)=>{
-                    $.each(data, (k, item)=>{
-                        console.log(item);
-                        var line = "<tr>";
-                            line += "<td>" + item.id + "</td>";
-                            line += "<td>" + item.title + "</td>";
-                            line += "<td>" + item.userId + "</td>";
-                            line += "</tr>";
-                        $("#tbnPosts").append(line);
-                    });
-                })
-                .fail((xhr, status, err)=>{
-
+            .done((data)=>{
+                $.each(data, (k, item)=>{
+                    console.log(item);
+                    var line = "<tr>";
+                        line += "<td>"+ item.id + "</td>";
+                        line += "<td><b>"+ item.title + "</b><br/>";
+                        line += item.body + "</td>";
+                        line += "<td> <button onClick='showDetails("+ item.id +");' > link </button> </td>";
+                        line += "</tr>";
+                    $("#tblPosts").append(line);
                 });
+                $("#main").show();
+            })
+            .fail((xhr, status, error)=>{
+            })
     }
+    $(()=>{
+        loadPosts();
+        $("#btnBack").click(()=>{
+            $("#main").show();
+        });
+    })
 </script>
 </html>
